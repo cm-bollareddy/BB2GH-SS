6c6dfea8288491e1e1e9ffb5f07c06ed3e743aa5
mchavez <mchavez@d7a1bf34-9008-0343-ae66-88e8bea8f563>
2015-01-23 20:05:57 +0000

    initial import of project
    git-svn-id: https://svn.hq.corp.pbs.org:8443/svn/PBS/Active%20Projects/NOLA%20Program%20Archive@21359 d7a1bf34-9008-0343-ae66-88e8bea8f563

---
+++ branch/OneLoginSAMLToolkit/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace OneLoginSAMLToolkit
+{
+    public class AppSettings
+    {
+        public string assertionConsumerServiceUrl = ConfigurationSettings.AppSettings["assertionConsumerServiceUrl"];
+        public string issuer = ConfigurationSettings.AppSettings["issuer"];
+        public string PartnerIdP = "PartnerIdP";
+    }
+}
